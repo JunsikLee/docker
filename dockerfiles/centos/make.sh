787c3320f85a0d0974096dba415c27a33d87949d
JunsikLee <junsik.lee.86@gmail.com>
2022-03-02 12:04:03 +0900

    add dockerfiles

---
+++ dockerfiles/centos/make.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "=> 도커 이미지 생성"
+docker build . -t centos-js
+
+echo "=> 도커 정지"
+docker stop centos-js
+
+echo "=> 도커 삭제"
+docker rm centos-js
+
+echo "=> 도커 생성"
+docker run --privileged -d  -p 1022:22 --name centos-js centos-js
---
+++ dockerfiles/centos/sh/setup-locale.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
